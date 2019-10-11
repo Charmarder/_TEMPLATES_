fdcc72da882cf3b76dc7524f50530bfed16d0132
Vyacheslav Yanchuk <vyacheslav.ianchuk@excelian.com>
2019-10-11 18:42:53 +0300

    Added Chocolatey script

---
+++ Windows/choco.ps1
@@ -0,0 +1,6 @@
+cinst mobaxterm -y          # MobaXTerm 12.2.0
+cinst postman -y            # Postman for Windows 7.8.0
+cinst puntoswitcher -y      # Punto Switcher 4.4
+cinst vscode -y             # Visual Studio Code 1.39.1
+cinst winrar -y             # WinRAR 5.7
+
