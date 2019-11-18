65e5412c2653a098220c40d08caf0d42ea9344be
Vyacheslav Yanchuk <vyacheslav.ianchuk@excelian.com>
2019-11-18 16:06:35 +0200

    updated choco.ps1

--- Windows/choco.ps1
+++ Windows/choco.ps1
@@ -1,3 +1,8 @@
+# this 2 need for Vim Vundle https://github.com/VundleVim/Vundle.vim/wiki/Vundle-for-Windows
+cinst -y git
+cinst -y curl
+cinst ctags --alow-empty-checksums -y   # it is needed for Vim taglist.vim plugin
+
 cinst mobaxterm -y          # MobaXTerm 12.2.0
 cinst postman -y            # Postman for Windows 7.8.0
 cinst puntoswitcher -y      # Punto Switcher 4.4
