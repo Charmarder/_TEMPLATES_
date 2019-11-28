fc0b9b466a4bb9e82495c581d6ea6813e2499fb5
Vyacheslav Yanchuk <vyacheslav.ianchuk@excelian.com>
2019-11-28 18:28:21 +0200

    Added .gitconfig

--- Windows/choco.ps1
+++ Windows/choco.ps1
@@ -1,11 +1,19 @@
 # this 2 need for Vim Vundle https://github.com/VundleVim/Vundle.vim/wiki/Vundle-for-Windows
-cinst -y git
-cinst -y curl
+cinst git -y                            # Git (Install) 2.24.0.2 (4,144,690)
+cinst curl -y                           # cURL 7.67.0 (1,333,002)
 cinst ctags --alow-empty-checksums -y   # it is needed for Vim taglist.vim plugin
 
-cinst mobaxterm -y          # MobaXTerm 12.2.0
-cinst postman -y            # Postman for Windows 7.8.0
-cinst puntoswitcher -y      # Punto Switcher 4.4
-cinst vscode -y             # Visual Studio Code 1.39.1
-cinst winrar -y             # WinRAR 5.7
+cinst strawberryperl -y             # Strawberry Perl 5.30.1.1 (756,344)
+cinst python3 -y                    # Python 3.x 3.8.0 (1,315,202)
+cinst nodejs.install -y             # Node JS (Install) 13.2.0 (3,076,632)
+cinst yarn -y                       # Yarn (Install) 1.19.2
 
+cinst vscode -y                     # Visual Studio Code 1.39.1 (734,874)
+cinst vim -y                        # Vim 8.1.2352 (359,644)
+cinst notepadplusplus.install -y    # Notepad++ (Install) 7.8.1 (4,785,376)
+
+cinst mobaxterm -y                  # MobaXTerm 12.2.0
+cinst postman -y                    # Postman for Windows 7.8.0
+cinst fiddler -y                    # Telerik Fiddler Web Debugger 5.0.20194.41348
+cinst puntoswitcher -y              # Punto Switcher 4.4
+cinst winrar -y                     # WinRAR 5.7
