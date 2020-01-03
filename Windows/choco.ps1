64498515907fa9b9627107cf947b9ebfd556c96b
Charmarder <slava.yanchuk@gmail.com>
2020-01-03 19:57:21 +0000

    edit choco.ps1

--- Windows/choco.ps1
+++ Windows/choco.ps1
@@ -1,17 +1,17 @@
 # this 2 need for Vim Vundle https://github.com/VundleVim/Vundle.vim/wiki/Vundle-for-Windows
 cinst git -y                            # Git (Install) 2.24.0.2 (4,144,690)
-cinst curl -y                           # cURL 7.67.0 (1,333,002)
+cinst curl -y                           # cURL 7.67.0 (1,368,204)
 cinst ctags --alow-empty-checksums -y   # it is needed for Vim taglist.vim plugin
 
-cinst strawberryperl -y             # Strawberry Perl 5.30.1.1 (756,344)
-cinst python3 -y                    # Python 3.x 3.8.0 (1,315,202)
-cinst nodejs.install -y             # Node JS (Install) 13.2.0 (3,076,632)
-cinst yarn -y                       # Yarn (Install) 1.19.2
+cinst strawberryperl -y             # Strawberry Perl 5.30.1.1 (767,799)
+cinst python3 -y                    # Python 3.8.1 (1,070,798)
+cinst nodejs.install -y             # Node JS (Install) 13.5.0 (3,138,285)
+cinst yarn -y                       # Yarn (Install) 1.21.1 (615,814)
 
-cinst vscode -y                     # Visual Studio Code 1.39.1 (734,874)
+cinst vscode -y                     # Visual Studio Code 1.41.1 (785,720)
 cinst vim -y                        # Vim 8.1.2352 (359,644)
-cinst notepadplusplus.install -y    # Notepad++ (Install) 7.8.1 (4,785,376)
-cinst db-visualizer -y              # DbVisualizer 10.0.17
+cinst notepadplusplus.install -y    # Notepad++ (Install) 7.8.2 (5,100,653)
+cinst db-visualizer -y              # DbVisualizer 10.0.17 (5,397)
 
 cinst mobaxterm -y                  # MobaXTerm 12.2.0
 cinst postman -y                    # Postman for Windows 7.8.0
