9136714c260bd094ab527191c497ed651f00004d
Charmarder <slava.yanchuk@gmail.com>
2020-01-16 21:01:42 +0200

    commited  latest NodeJS/JS files

---
+++ _playground/NodeJS/TypeScript/HelloWorld.js
@@ -0,0 +1,6 @@
+"use strict";
+function sayHello(name) {
+    console.log("Hello " + name + "!");
+}
+sayHello('Dave');
+sayHello('Slava');
---
+++ _playground/NodeJS/TypeScript/HelloWorld.ts
@@ -0,0 +1,6 @@
+function sayHello(name: string): void {
+    console.log(`Hello ${name}!`);
+  }
+  
+  sayHello('Dave');
+  sayHello('Slava');
