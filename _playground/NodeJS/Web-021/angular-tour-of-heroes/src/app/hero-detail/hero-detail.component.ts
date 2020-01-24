57ae4749646469ff8c9141c92232aafe03ecc787
Charmarder <slava.yanchuk@gmail.com>
2020-01-24 13:53:39 +0200

    Web-021 - Angular-Tour-of-Heroes (1-6)

--- _playground/NodeJS/Web-021/angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ _playground/NodeJS/Web-021/angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -31,4 +31,8 @@ export class HeroDetailComponent implements OnInit {
   goBack(): void {
     this.location.back();
   }
+
+  save(): void {
+    this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
+  }
 }
